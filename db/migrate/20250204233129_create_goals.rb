df41b2cdc223d4b883f9d4839665bf04cdfb0fd3
leila <143362178+1e1104k4@users.noreply.github.com>
2025-02-05 14:29:19 -0600

    chore: create table goals migration

--- db/migrate/20250204233129_create_goals.rb
+++ db/migrate/20250204233129_create_goals.rb
@@ -1,9 +1,9 @@
 class CreateGoals < ActiveRecord::Migration[8.0]
   def change
     create_table :goals do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :description
-      t.integer :status
+      t.integer :status, null: false, default: 0
 
       t.timestamps
     end
