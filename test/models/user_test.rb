b9a92e006e89214c17060a2c1b6ab5d3a208f99f
leila <143362178+1e1104k4@users.noreply.github.com>
2025-02-22 21:28:48 -0600

    chore: user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "user name should be unique" do
+    user = users(:one)
+    new_user = User.new(name: user.name)
+    assert new_user.invalid?
+    assert new_user.errors[:name].include?("has already been taken")
+  end
 end
