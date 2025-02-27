c8f90410950af1a771fcfcea7229e65e35b38b14
leila <143362178+1e1104k4@users.noreply.github.com>
2025-02-27 13:55:48 -0600

    feat: add ruby ui

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "tailwindcss-animate" # @1.0.7
+pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @4.0.9
+pin "motion", to: "https://cdn.jsdelivr.net/npm/motion@11.11.17/+esm"
+
